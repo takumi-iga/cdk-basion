60eb9fb3966ce78aa4e2b8bf967e880c58425afd
igarashi.takumi <igarashi.takumi@i3design.co.jp>
2023-08-15 12:26:01 +0900

    basion server

--- lib/cdk-basion-stack.ts
+++ lib/cdk-basion-stack.ts
@@ -1,13 +1,36 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from 'aws-cdk-lib'
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import { Construct } from 'constructs'
 
 export class CdkBasionStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    
+    const vpc = new ec2.Vpc(this, 'basionVPC', {
+      cidr: '10.0.0.0/16',
+      maxAzs: 1,
+      subnetConfiguration: [
+        {
+          name: 'public-basion-subnet',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+      ],
+    })
+
+    // Security Group
+    const basionSG = new ec2.SecurityGroup(this, 'basionSG', {
+      vpc,
+      allowAllOutbound: true,
+    })
+    basionSG.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH from anywhere')
+
+    // EC2 Instance
+    const basionServer = new ec2.BastionHostLinux(this, 'basionServer', {
+      vpc,
+      subnetSelection: { subnetType: ec2.SubnetType.PUBLIC },
+      securityGroup: basionSG,
+    })
+    basionServer.allowSshAccessFrom(ec2.Peer.anyIpv4())
   }
 }
